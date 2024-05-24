93571846763579b1e0788319a9e7abeb79b9ec53
Gustavo Henrique <113194640+GustaHz1@users.noreply.github.com>
2024-05-24 20:19:59 -0300

    Delete Dragão.py

--- Dragão.py
+++
@@ -1,8 +0,0 @@
-c = int(input("Quantas operações deseja realizar: "))
-for i in range(c):
-    n = int(input("Digite um valor: "))
-    if n > 8000 or n == 8000:
-        print("Mais de 8000!")
-    else:
-        print("Inseto!")
-        
